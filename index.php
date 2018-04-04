1b199df9b3ce00166a89a1f5fd112c455ca43d02
donatien513 <31708604+donatien513@users.noreply.github.com>
2018-04-04 07:57:51 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,3 @@
-<?php
-	if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message'])) {
-	       // from the form
-	       $name = trim(strip_tags($_POST['name']));
-	       $email = trim(strip_tags($_POST['email']));
-	       $message = htmlentities($_POST['message']);
-
-	       // set here
-	       $subject = "Message from the Online CV!";
-	       $to = 'donatiennambinintsoa@gmail.com';
-
-	       $body = <<<HTML
-	$message
-	HTML;
-
-	       $headers = "From: $email\r\n";
-	       $headers .= "Content-type: text/html\r\n";
-
-	       // send the email
-	       mail($to, $subject, $body, $headers);
-
-	       // redirect afterwords, if needed
-	       header('Location: index.php');
-	}
-?>
 <!DOCTYPE html>
 <html lang="fr">
 	<head>
@@ -317,18 +292,6 @@
 						</tr>
 					</tbody>
 				</table>
-				<form action="" method="post">
-				  <label for="name">Name:</label>
-				  <input type="text" name="name" id="name" />
-
-				  <label for="Email">Email:</label>
-				  <input type="text" name="email" id="email" />
-
-				  <label for="Message">Message:</label><br />
-				  <textarea name="message" rows="20" cols="20" id="message"></textarea>
-
-				  <input type="submit" name="submit" value="Submit" />
-				</form>
 			</div>
 		</div>
 		<script type="application/javascript">
