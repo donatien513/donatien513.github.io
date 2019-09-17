2341a7fba62fe4e09d0eb8d6e1e52af4a2edab22
Donatien <donqtiennambinintsoa@gmail.com>
2019-09-17 20:57:07 +0300

    update

--- components/banner.tsx
+++ components/banner.tsx
@@ -19,24 +19,34 @@ class Banner extends React.Component {
 
   render() {
     return(
-      <div className="w-100 h-100">
+      <div className="w-100 mb5 relative">
+        <div className="w-100 h-100 absolute z--1 bg-navy"></div>
+        <Wave className="w-100 h6 absolute z--1 upside-down" fill='#34eb9e'
+          paused={false}
+          options={{
+            height: 49,
+            amplitude: 85,
+            speed: 0.09,
+            points: 7
+          }}
+        />
         <Wave className="w-100 h6 absolute z--1 upside-down" fill='#594fe8'
           paused={false}
           options={{
-            height: 30,
+            height: 70,
             amplitude: 45,
             speed: 0.15,
             points: 5
           }}
         />
-        <div className="h-100 w-100 z-1">
-          <Jumbotron fluid className="bg-transparent">
+        <div className="w-100 z-1">
+          <Jumbotron fluid className="bg-transparent pb0">
             <div className="w-100">
               <Container className="h-100 ph4">
-                <h1 className="display-3 lh-solid mv-0 pacifico always-clear">Bonjour</h1>
-                <h3 className="f3-l lh-solid mv-0 roboto-condensed">Je conçois des applications&nbsp;
-                <span className="light-purple">
-                  <Typed 
+                <h1 className="display-3 lh-solid mv-0 monospace white">Tpkl</h1>
+                <h3 className="f3-l lh-solid mv-0 white roboto-condensed">Je conçois des applications&nbsp;
+                <span className="light-blue">
+                  <Typed
                     strings={[
                       'webs...',
                       'mobiles...',
@@ -49,10 +59,17 @@ class Banner extends React.Component {
                     loop />
                 </span>
                 </h3>
-                <p className="roboto">
-                  Donatien NAMBININTSOA ANDRIANIRINA, <span className="gray">développeur Full Stack</span>
+                <p className="roboto white">
+                  Donatien NAMBININTSOA ANDRIANIRINA, <span className="near-white">développeur Full Stack</span>
                 </p>
-<SyntaxHighlighter className="mv5 mw7 w-100 center f5" language="javascript">{
+              </Container>
+            </div>
+          </Jumbotron>
+          <Jumbotron className="w-100 ph0 pv0 br0 relative bg-white z--1">
+            <div className="absolute bg-navy w-100 h-50 z--1">aaa</div>
+            <Container>
+              <div>
+<SyntaxHighlighter className="mv5 mw7 w-100 center shadow f4 f6-sm bg-white" language="javascript">{
 `const network = require("awesome-recruitment-network");
 const { skills, expectedSalary } = require("./me");
 
@@ -67,8 +84,8 @@ network.on("opportunity", (job, recruiter) => {
 
 `
 }</SyntaxHighlighter>
-              </Container>
-            </div>
+              </div>
+            </Container>
           </Jumbotron>
         </div>
       </div>
