fd169b425d8b4d9185175d6ee354e658faad0a40
Donatien <donatiennambinintsoa@gmail.com>
2019-09-18 14:29:11 +0300

    Update Deploy script

--- gh-pages-deploy.js
+++ gh-pages-deploy.js
@@ -1,12 +1,27 @@
 /* eslint-disable */
 
+const url = require('url');
 const ghpages = require('gh-pages');
+const packageJson = require('./package');
+const GithubToken = process.env.GH_TOKEN
+
+/**
+ * Extract repo url from package.json, then add auth in it
+ */
+const getRepositoryURL = () => {
+  const repoUrl = packageJson.repository.url;
+  const parsedRepoURL = url.parse(repoUrl);
+  parsedRepoURL.protocol = 'https';
+  parsedRepoURL.auth = process.env.GH_TOKEN;
+  return url.format(parsedRepoURL)
+}
 
 ghpages.publish('out', {
+  repo: getRepositoryURL(),
   dotfiles: true,
   message: 'Deploy to Github Page',
   user: {
-    name: 'Donatien NAMBININTSOA (Circle CI)',
+    name: 'Donatien NAMBININTSOA (CI)',
     email: 'donatiennambinintsoa@gmail.com'
   }
 }, (error, message) => {
